6f66f58234a9133e814102473ae3a5b93b046fe6
Christiano Donke <chrido@microsoft.com>
2020-03-12 08:53:10 -0300

    Welcome message

--- IntermediatorBotSample/Bot/IntermediatorBot.cs
+++ IntermediatorBotSample/Bot/IntermediatorBot.cs
@@ -30,7 +30,19 @@ namespace IntermediatorBotSample.Bot
             // Save any state changes that might have occured during the turn.
             await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
-        
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello {member.Name}"), cancellationToken);
+                }
+            }
+        }
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
